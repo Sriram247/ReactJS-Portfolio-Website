1ecad12fa931339846e609e1803bb22eeb32ffee
sriram <sriramachandran28@gmail.com>
2024-10-30 12:13:38 -0300

    final faetures

---
+++ src/Home.js
@@ -0,0 +1,243 @@
+// Home.js
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+
+import './App.css';
+import './Body.css';
+
+function Home() {
+    
+    const [position, setPosition] = useState({ x: 0, y: 0, body:0 }); //Body-0 means its a circle, Body-1 means its on an image
+    const [isHoveredSocialMedia, setIsHoveredSocialMedia] = useState(false);
+    const [hoveredIndex, setHoveredIndex] = useState(null); 
+    const [cursorEdit, setCursorEdit] = useState(true); //true is small,false is big
+    const [cursorVisible, setCursorVisible] = useState(true); 
+    const [positionOfImage, setPositionofImage] = useState({ top: 0, left: 0,bottom:0, right:0}); 
+    const relativeImageRef = useRef(null);
+    
+    //Change to about:
+    const[page,setPage] = useState("Home");
+
+    useEffect(() => {
+      const element = relativeImageRef.current;
+      const rect = element.getBoundingClientRect();
+      setPositionofImage({top: rect.top, left: rect.left, bottom:rect.bottom, right: rect.right})
+      
+      console.log("Top:", rect.top); // Position relative to the viewport
+      console.log("Left:", rect.left); // Position relative to the viewport
+    },[position]);
+    
+
+    useEffect(() => {
+      // Function to update the state with the mouse position
+      const handleMouseMove = (e) => {
+        setPosition({ x: e.pageX, y: e.pageY });
+
+        console.log(positionOfImage.left);
+        const { clientX, clientY } = e;
+        if (
+          clientX >= positionOfImage.left &&
+          clientX <= positionOfImage.right &&
+          clientY >= positionOfImage.top &&
+          clientY <= positionOfImage.bottom
+        ){
+          setCursorVisible(false);  // Hide cursor
+          console.log("Cursor visible is false");
+        }
+        else{
+          console.log("Cursor visible is released");
+          setCursorVisible(true); 
+
+        }
+          console.log(e.pageX, e.pageY);
+        };
+        
+      window.addEventListener('mousemove', handleMouseMove);
+  
+      return () => {
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }, []);
+
+
+    
+
+      // Style object to position the follower div
+    const followerStyle = {
+      position: 'absolute',
+      left: `${position.x}px`,
+      top: `${position.y}px`,
+      width: '60px',
+      height: '60px',
+      backgroundColor: 'transparent',
+      borderRadius: '50%',
+      pointerEvents: 'none', 
+      transform: 'translate(-50%, -50%)', 
+      zIndex:100,
+      transition: 'opacity 0.2s ease ,width 0.3s ease, height 0.3s ease, transform 0.3s ease', 
+
+      
+    };
+    const linkStyle = {
+      position: 'absolute',
+      left: `${position.x}px`,
+      top: `${position.y}px`,
+      width: '90px',
+      height: '90px',
+      backgroundColor: 'White',
+      borderRadius: '50%',
+      pointerEvents: 'none', 
+      transform: 'translate(-50%, -50%)', 
+      zIndex:100,
+      //add smoother transition
+      transition: 'width 0.5s ease, height 0.5s ease, transform 0.5s ease', 
+
+    };
+    const cursorGone = {
+      position: 'absolute',
+      left: `${position.x}px`,
+      top: `${position.y}px`,
+      width: '200px',
+      height: '200px',
+      backgroundColor: 'transparent',
+      borderRadius: '50%',
+      pointerEvents: 'none', 
+      transform: 'translate(-50%, -50%)', 
+      zIndex:100,
+      opacity: '0',
+      //add smoother transition
+      transition: 'opacity 0.3s ease, width 0.2s ease, height 0.2s ease', 
+    };
+
+  const none = {};
+    
+
+
+
+    
+    // Array of items
+    const items = ['Home', 'About'];
+    const routeMap = {
+        Home: '/',
+        About: '/About',
+      };
+
+      
+    // Handle when mouse enters a list item
+    const handleMouseEnter = (index) => {
+        setHoveredIndex(index); // Set the hovered index to the current item
+        setCursorEdit(false); // Set the hovered index to the current item
+
+    };
+    
+    // Handle when mouse leaves a list item
+    const handleMouseLeave = () => {
+        setHoveredIndex(null); // Reset the hovered index when the mouse leaves
+        console.log("Mouse became small");
+        setCursorEdit(true); // Set the hovered index to the current item
+    };
+
+    const handleMouseEnterLogo = () => {
+      setCursorEdit(false);
+      console.log("Mouse entered logo")
+    }
+
+    const handleMouseLeaveLogo = () => {
+      setCursorEdit(true);
+      console.log("Mouse became small");
+
+    }
+
+    const handleMouseEnterSocialMedia = () => {
+      setCursorEdit(false);
+      setIsHoveredSocialMedia(true);
+    };
+  
+    const handleMouseLeaveSocialMedia = () => {
+      setCursorEdit(true);
+      console.log("Mouse became small");
+
+      setIsHoveredSocialMedia(false);
+    };
+
+  return (
+    <>
+    <div id="cursor" style={cursorEdit === false ? linkStyle : (cursorVisible === false ? cursorGone : followerStyle)}>
+    </div>
+
+    
+        <div className="navbar-container">
+            <div className="logo">
+                <a href="#"
+                onMouseEnter={() => handleMouseEnterLogo()}
+                onMouseLeave={handleMouseLeaveLogo}>SR</a>
+            </div>
+
+            <div>
+            <ul className="nav-links">
+                {items.map((item, index) => (
+                  <li
+                  key={index}
+                  className={hoveredIndex === index ? 'link-grow' : ''}
+                  onMouseEnter={() => handleMouseEnter(index)} 
+                  onMouseLeave={handleMouseLeave}   
+                  >
+                    <Link to={routeMap[item]}>
+                        {item}
+                    </Link>
+                    </li>
+                ))}
+            </ul>
+                </div>
+           
+
+            <div className="social-media" >
+                
+                <a className={`socialMedia ${isHoveredSocialMedia ? "hovered" : ""}`}
+                onMouseEnter={() => handleMouseEnterSocialMedia()}
+                onMouseLeave={handleMouseLeaveSocialMedia}
+                href="https://github.com/Sriram247">Github</a>
+
+                <a className={`socialMedia ${isHoveredSocialMedia ? "hovered" : ""}`}
+                onMouseEnter={() => handleMouseEnterSocialMedia()}
+                onMouseLeave={handleMouseLeaveSocialMedia}
+                href="https://www.linkedin.com/in/sriramramesh28/">LinkedIn</a>
+
+            </div>
+        </div>
+
+    
+        <div className="body">
+        <div className='text'>
+
+        <h3>
+            Hi, I'm Sriram Ramesh
+        </h3>
+        <h2>
+            An Aspiring Entrepreneur trying react!<br></br>
+            Based in Halifax, Canada.
+        </h2>
+        
+        </div>
+        <div className="image">
+        
+        <img ref={relativeImageRef} className="background-image" src={`${process.env.PUBLIC_URL}/hero-pic.png`} alt="My Portfolio Screenshot" />
+        <img 
+          className="foreground-image" 
+          src={`${process.env.PUBLIC_URL}/hero-pic-bw.png`} 
+          alt="My Portfolio Screenshot" 
+          style= {{clipPath: `circle(100px at ${position.x - positionOfImage.left}px ${position.y - positionOfImage.top}px)`}}/>
+         
+
+        </div>
+        </div>
+  
+
+
+    
+    </>
+  );
+
+}
+
+export default Home;
---
+++ src/Weather.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+const Weather = () => {
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [isCelsius, setIsCelsius] = useState(true); // State to track if temperature is in Celsius or Fahrenheit
+    const units = isCelsius ? 'metric' : 'imperial';
+    const [hover, setHover] = useState(false);
+
+    const apiKey = '4570dc19e354854bbc203bdfc809add5';
+    const city = 'Halifax';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            try {
+                const response = await fetch(apiUrl);
+                if (!response.ok) {
+                    throw new Error(`Error: ${response.statusText}`);
+                }
+                const data = await response.json();
+                setWeatherData(data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        };
+
+        fetchWeather();
+    }, [apiUrl]);
+
+    const toggleUnits = () => {
+        setIsCelsius(!isCelsius);
+    };
+
+    if (loading) return <p>Loading weather data...</p>;
+    if (error) return <p>Error loading weather data: {error}</p>;
+
+
+    const buttonStyle = {
+        backgroundColor: hover ? '#333' : 'black',
+        color: 'white',
+        border: 'none',
+        padding: '10px 20px',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        
+        borderRadius: '5px',
+        cursor: 'pointer',
+        transform: hover ? 'scale(1.05)' : 'scale(1)',
+        transition: 'background-color 0.3s ease, transform 0.2s ease',
+    };
+
+
+    return (
+        <div className="weather-container">
+            <h4>Temperature: {weatherData.main.temp}°{isCelsius ? 'C' : 'F'}</h4>
+
+            <button
+        style={buttonStyle}
+        onClick={toggleUnits}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        >
+                Switch to {isCelsius ? 'Fahrenheit' : 'Celsius'}
+            </button>
+        </div>
+    );
+};
+
+export default Weather;
